bca3d24dae4b68aa010ce57fdafc9654d2fd3222
AdeyinkaAnibire <adb>
2022-03-10 15:11:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * 
  * Return: Always 0 (Success)
  */
-int main(void)
+	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzel")
+
 		return (0);
 }
