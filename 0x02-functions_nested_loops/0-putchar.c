f4938b28a1cebabce18ecd168d1c62bd8a9581c4
AdeyinkaAnibire <adb>
2022-03-15 14:02:29 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char holberton[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(holberton[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
