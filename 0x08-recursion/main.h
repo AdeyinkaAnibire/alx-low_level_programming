e02d2b3ca87c1cebf909657bd3454fe32b9c0b67
AdeyinkaAnibire <adb>
2022-03-30 04:43:20 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,17 +1,17 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-int _putchar(char c);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
+int wildcmp(char *s1, char *s2);
+int is_prime_number(int n);
+int is_palindrome(char *s);
 
 #endif
 
