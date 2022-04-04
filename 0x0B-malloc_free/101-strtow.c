5ce924c26c0ba05a72a035bdebd7f5ae360b43de
AdeyinkaAnibire <adb>
2022-04-04 06:06:37 -0700

    Funtion that splits a string into two words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -55,5 +55,14 @@ char **strtow(char *str)
 					ch_free_grid(aout, i);
 					return (NULL);
 				}
-
+				break;
+			}
+		}
+		for (j = 0; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
+	}
+	aout[i] = NULL;
+	return (aout);
+}
 
